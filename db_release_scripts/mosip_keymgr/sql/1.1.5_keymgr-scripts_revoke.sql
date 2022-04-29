d587ff7102feed91a813512f97ec07609d9f2212
Keshav Mishra <chandrakeshavmishra@gmail.com>
2022-04-29 12:43:58 +0530

    Update 1.1.5_keymgr-scripts_revoke.sql

--- db_release_scripts/mosip_keymgr/sql/1.1.5_keymgr-scripts_revoke.sql
+++ db_release_scripts/mosip_keymgr/sql/1.1.5_keymgr-scripts_revoke.sql
@@ -1,9 +1,9 @@
 -- -------------------------------------------------------------------------------------------------
 -- Database Name	: mosip_keymgr
--- Release Version 	: 1.1.5
+-- Release Version 	: 1.1.5.5
 -- Purpose    		: Revoking Database Alter deployement done for release in Key manager DB.       
--- Create By   		: Sadanandegowda DM
--- Created Date		: Feb-2022
+-- Create By   		: Chandra Keshav Mishra
+-- Created Date		: APRIL-2022
 -- 
 -- Modified Date        Modified By         Comments / Remarks
 -- -------------------------------------------------------------------------------------------------
